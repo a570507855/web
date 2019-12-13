3ded16e807355d0bdf8082be9366770739a610f2
许亦勇 <570507855@qq.com>
2019-12-13 15:41:41 +0800

    'asd'

--- web/WEB-INF/js/base.js
+++ web/WEB-INF/js/base.js
@@ -23,17 +23,69 @@
         $(window).resize(function () {
         })
 
-        //解析URL
-        function getRequest() {
-            var url = window.location.search; //获取url中"?"符后的字串
-            var theRequest = new Object();
-            if (url.indexOf("?") !== -1) {
-                var str = url.substr(1);
-                var strs = str.split("&");
-                for(var i = 0; i < strs.length; i ++) {
-                    theRequest[strs[i].split("=")[0]]=decodeURI(strs[i].split("=")[1]);
-                }
-            }
-            return theRequest;
+
+
+    }
+
+    //设置cookie
+    function setCookie(cookies, expireDay, path, domain) {
+        var str  = cookies
+        var time = "";
+        if(expireDay){
+            var date=new Date();
+            date.setTime(date.getTime()+expireDay*24*3600*1000);
+            time = ';expires='+date.toUTCString()
         }
+        str += time;
+        if(path){
+            str += ';path='+path
+        }
+        if(domain){
+            str += ';domain='+domain
+        }
+        document.cookie = str
+    }
+
+    //获取cookie
+    function getCookie() {
+        var cookies = document.cookie,
+            cookieObj = {};
+        if(cookies != '' && cookies != undefined){
+            var cookiesArr = cookies.split(';')
+            for(var i = 0; i != cookiesArr.length;++i){
+                var key = cookiesArr[i].split('=')[0],
+                    value = cookiesArr[i].split('=')[1];
+                cookieObj[key.trim()] = value.trim()
+            }
+        }
+        return cookieObj;
+    }
+
+    //删除cookie
+    function delCookie(cookie,path, domain) {
+        var date = new Date(),
+            str = cookie;
+        date.setTime(date.getTime()-1000);
+        var time = date.toUTCString()
+        str += ';expires='+time
+        if(path){
+            str += ';path='+path
+        }
+        if(domain){
+            str += ';domain='+domain
+        }
+        document.cookie = str
+    }
+    //解析URL
+    function getRequest() {
+        var url = window.location.search; //获取url中"?"符后的字串
+        var theRequest = new Object();
+        if (url.indexOf("?") !== -1) {
+            var str = url.substr(1);
+            var strs = str.split("&");
+            for(var i = 0; i < strs.length; i ++) {
+                theRequest[strs[i].split("=")[0]]=decodeURI(strs[i].split("=")[1]);
+            }
+        }
+        return theRequest;
     }
--- web/WEB-INF/views/main/book/book.jsp
+++ web/WEB-INF/views/main/book/book.jsp
@@ -5,31 +5,56 @@
     <script src="../../../js/base.js"></script>
 </head>
 <body>
-    <h1>书籍查询</h1>
+<div style="width: 95%;margin: 60px;">
+    <div >
+        <h1>设置cookie</h1>
+        <p>document.cookie = "userName=许亦勇;password=disueb11"</p>
+    </div>
+    <div >
+        <h1>获取cookie</h1>
+        <p>document.cookie 获得以分号分割的键值对字符串</p>
+    </div>
+    <div >
+        <h1>给cookie设置终止日期</h1>
+        <p>document.cookie="userId=828; expires=GMT_String"</p>
+    </div>
+    <div >
+        <h1>删除cookie</h1>
+        <p>document.cookie="userId=828; expires=GMT_String" 将时间设置为过去时间便可删除cookie</p>
+    </div>
+    <div >
+        <h1>指定可访问cookie的路径</h1>
+        <p>document.cookie="userId=320; path=/shop"; 表示当前cookie仅能在shop目录下使用，path=/表示整个网站可用</p>
+    </div>
+    <div >
+        <h1>指定可访问cookie的主机名</h1>
+        <p>document.cookie="userId=320; domain=cookieDomain"; 表示当前cookie能在同一个域下的不同主机使用</p>
+    </div>
+</div>
 </body>
 <script>
-    $.ajax({
-        url:'/book/delete',
-        data:{
-            id:"3,4,6",
-        },
-        async:false,
-        success:function (res) {
-            console.log(res)
-        }
-    })
-
-
     // $.ajax({
-    //     url:'/book/selectList',
+    //     url:'/book/delete',
     //     data:{
-    //
+    //         id:"3,4,6",
     //     },
+    //     async:false,
     //     success:function (res) {
-    //         console.log("分页查询：")
     //         console.log(res)
     //     }
     // })
+    console.log('${sessionScope.username}')
+    console.log(getCookie())
+    $.ajax({
+        url:'/book/selectList',
+        data:{
+
+        },
+        success:function (res) {
+            console.log("分页查询：")
+            console.log(res)
+        }
+    })
 
 </script>
 </html>
